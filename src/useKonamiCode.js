090c7c3a8a9499f55101a610bd33534226c940cf
Kevin Viglucci <kviglucci@gmail.com>
2021-01-18 22:50:05 -0600

    remove unused files

--- src/useKonamiCode.js
+++ src/useKonamiCode.js
@@ -1,45 +1,52 @@
-import { useEffect } from 'react';
-import { useMachine } from '@xstate/react';
-import { inspect } from '@xstate/inspect';
-import konamiCodeMachine from './konami-code-fsm';
+import { useEffect } from "react";
+import { useMachine } from "@xstate/react";
+import konamiCodeMachine from "./konami-code-fsm";
 
-inspect({
-    url: 'https://statecharts.io/inspect',
-    iframe: false
-});
+// uncomment to enable xstate debug
+// import { inspect } from '@xstate/inspect';
+
+// uncomment to enable xstate debug
+// inspect({
+//     url: 'https://statecharts.io/inspect',
+//     iframe: false
+// });
 
 function useKonamiCode() {
-    const [currentState, send] = useMachine(konamiCodeMachine, { devTools: true });
+    const [currentState, send] = useMachine(konamiCodeMachine, {
+        devTools: true
+    });
 
     useEffect(() => {
-        const onKeyUp = (e) => send({
-            type: 'KEYUP', keyName: getKeyName(e.keyCode)
-        });
+        const onKeyUp = (e) =>
+            send({
+                type: "KEYUP",
+                keyName: getKeyName(e.keyCode)
+            });
 
         // When a key is pressed send an event to the machine
-        window.document.addEventListener('keyup', onKeyUp);
+        window.document.addEventListener("keyup", onKeyUp);
 
         // Cleanup event handler if hook is deregistered
-        return () => window.document.removeEventListener('keyup', onKeyUp);
+        return () => window.document.removeEventListener("keyup", onKeyUp);
     }, [send]);
 
     useEffect(() => {
-        if (currentState.matches('codeComplete')) {
-            send({ type: 'RESET' });
+        if (currentState.matches("codeComplete")) {
+            send({ type: "RESET" });
         }
     }, [currentState, send]);
 
-    return currentState.matches('codeComplete');
+    return currentState.matches("codeComplete");
 }
 
 const getKeyName = (keyCode) => {
     return {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        65: 'A',
-        66: 'B',
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
+        65: "A",
+        66: "B"
     }[keyCode];
 };
 
