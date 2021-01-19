13cfe90a928ea4629f9b9bc176281f752fd9fc88
Kevin Viglucci <kviglucci@gmail.com>
2021-01-18 22:51:52 -0600

    add editorconfig update styling

--- src/useKonamiCode.js
+++ src/useKonamiCode.js
@@ -12,42 +12,42 @@ import konamiCodeMachine from "./konami-code-fsm";
 // });
 
 function useKonamiCode() {
-    const [currentState, send] = useMachine(konamiCodeMachine, {
-        devTools: true
-    });
+  const [currentState, send] = useMachine(konamiCodeMachine, {
+    devTools: true
+  });
 
-    useEffect(() => {
-        const onKeyUp = (e) =>
-            send({
-                type: "KEYUP",
-                keyName: getKeyName(e.keyCode)
-            });
+  useEffect(() => {
+    const onKeyUp = (e) =>
+      send({
+        type: "KEYUP",
+        keyName: getKeyName(e.keyCode)
+      });
 
-        // When a key is pressed send an event to the machine
-        window.document.addEventListener("keyup", onKeyUp);
+    // When a key is pressed send an event to the machine
+    window.document.addEventListener("keyup", onKeyUp);
 
-        // Cleanup event handler if hook is deregistered
-        return () => window.document.removeEventListener("keyup", onKeyUp);
-    }, [send]);
+    // Cleanup event handler if hook is deregistered
+    return () => window.document.removeEventListener("keyup", onKeyUp);
+  }, [send]);
 
-    useEffect(() => {
-        if (currentState.matches("codeComplete")) {
-            send({ type: "RESET" });
-        }
-    }, [currentState, send]);
+  useEffect(() => {
+    if (currentState.matches("codeComplete")) {
+      send({ type: "RESET" });
+    }
+  }, [currentState, send]);
 
-    return currentState.matches("codeComplete");
+  return currentState.matches("codeComplete");
 }
 
 const getKeyName = (keyCode) => {
-    return {
-        37: "left",
-        38: "up",
-        39: "right",
-        40: "down",
-        65: "A",
-        66: "B"
-    }[keyCode];
+  return {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    65: "A",
+    66: "B"
+  }[keyCode];
 };
 
 export default useKonamiCode;
